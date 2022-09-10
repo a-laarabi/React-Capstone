7245f862bba9f0c6b9b8622b8b77fe71090001e4
a-laarabi <contact.laarabi@gmail.com>
2022-09-10 02:10:53 +0100

    fixing linters

--- src/__test__/HomeHeader.test.js
+++ src/__test__/HomeHeader.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import store from '../redux/store';
-import HomeHeader from '../components/HomeHeader'
+import HomeHeader from '../components/HomeHeader';
 
 describe('Test the components', () => {
   it('Check if the HomeHeader.js file\'s section are available', () => {
--- src/__test__/gameDetail.test.js
+++ src/__test__/gameDetail.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import store from '../redux/store';
-import GameDetail from '../components/GameDetail'
+import GameDetail from '../components/GameDetail';
 
 describe('Test the components', () => {
   it('Check if the GameDetail.js file\'s section are available', () => {
